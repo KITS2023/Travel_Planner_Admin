94ae7b0bf568dd8c1d5332b6fad9bd93fdfced1c
LinhNguyen2403 <vulinh.16195@gmail.com>
2023-08-15 17:33:47 +0700

    downgrade to react router dom v5

--- src/main.jsx
+++ src/main.jsx
@@ -1,34 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import ErrorPage from './ErrorPage.jsx';
-import App from './App.jsx';
-import LoginForm from './Login.jsx';
-import RegisterForm from "./Register.jsx";
-import ForgotPasswordPage from "./ForgotPasswordPage.jsx";
-import './index.css';
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import "./index.css";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    errorElement: <ErrorPage />
-  },
-  {
-    path: "login",
-    element: <LoginForm />
-  },
-  {
-    path: "register",
-    element: <RegisterForm />
-  },
-  {
-    path: "forgot-password",
-    element: <ForgotPasswordPage />
-  }
-])
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+);
